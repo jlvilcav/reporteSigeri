d08bfec3a89131d3de11109c01a006854860a114
Jorge Vilca <jlvilcav@gmail.com>
2016-11-29 17:26:03 -0500

    Se mejoro los estilos para la minima resolución, los datefield buttons y la tabla

--- js/app.js
+++ js/app.js
@@ -1,19 +1,9 @@
 $(document).ready(function(){
    $("#dtDesde1").datetimepicker({
-        minDate: 0,
-        maxDate: "+60D",
-        numberOfMonths: 2,
-        onSelect: function(selected) {
-          $("#txtToDate").datepicker("option","minDate", selected)
-        }
+        
     });
 
     $("#dtHasta2").datetimepicker({
-        minDate: 0,
-        maxDate:"+60D",
-        numberOfMonths: 2,
-        onSelect: function(selected) {
-           $("#txtFromDate").datepicker("option","maxDate", selected)
-        }
+        
     });
 });
